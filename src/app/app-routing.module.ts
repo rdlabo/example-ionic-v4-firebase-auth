0c8afde5bbe2b09d96210cd184dba7c88fe228f0
榊原昌彦 <sakakibara@rdlabo.jp>
2018-08-10 18:16:12 +0900

    create pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'signup', loadChildren: './auth/signup/signup.module#SignupPageModule' },
+  { path: 'signin', loadChildren: './auth/signin/signin.module#SigninPageModule' },
+  { path: 'reset_password', loadChildren: './auth/reset-password/reset-password.module#ResetPasswordPageModule' },
 ];
 
 @NgModule({
