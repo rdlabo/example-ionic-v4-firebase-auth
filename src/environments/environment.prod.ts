550577add91c1a2f463ecbfad7ee111c29cd0ef7
榊原昌彦 <sakakibara@rdlabo.jp>
2018-08-11 13:26:25 +0900

    add README

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDCNOtv0PSzqkIkjtQe6XxOpfFQG43wsDE',
-    authDomain: 'aia-airlec.firebaseapp.com',
-    databaseURL: 'https://aia-airlec.firebaseio.com',
-    projectId: 'aia-airlec',
-    storageBucket: 'aia-airlec.appspot.com',
-    messagingSenderId: '631676390465',
+    // past firebase setting
   },
 };
